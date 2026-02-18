c44dc1c989bb627d065e510f4f44ec71aac91efd
# nziokimutisya54-commit <nziokimutisya58@gmail.com>
2026-02-18 11:05:16 +0300

    msg

---
+++ pythonbasic/dictionary.py
@@ -0,0 +1,17 @@
+# benedict musa
+# 18/2/2026
+# program to show dictionaries in python
+
+
+car = {"Model" : "Audi","Make" : "Colour"  "Cherry","Year" : "2025"}
+print(car)
+
+print(car)
+print(car)
+
+students = {"Alice": 24, "James" : 18, "Mark" : 22, "Daisy" : 19}
+
+for key in students:
+    print(key)
+for val in students.values():
+    print(val)    
---
+++ pythonbasic/tuples.py
@@ -0,0 +1,18 @@
+# benedict musa
+# 13/2.2026
+# program to show for tuples in physic
+#tuples of fruits
+
+fruits =  ("Avocado","Kiwi","Apples","Bananas","Oranges")
+
+print(len(fruits))
+print(fruits[0])
+print(fruits[4])
+print(fruits[-1])
+print(fruits[-5])
+
+
+fruits_list = list(fruits)
+
+fruits_list.append("Guava")
+print(fruits_list)
